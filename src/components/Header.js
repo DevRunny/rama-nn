8b1b2a96fd4b80828fc7e934ccce37954336b303
Max Lebedev <dev.runny@gmail.com>
2022-01-05 21:52:28 +0300

    Сборка проекта

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from "../logo.svg";
+
+const Header = (props) => (
+  <header>
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>Hello World</h1>
+  </header>
+);
+
+export default Header;
