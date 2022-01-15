6f3208f79b94246fc72fe4b27895c6c44e7495dd
Max Lebedev <dev.runny@gmail.com>
2022-01-15 18:38:49 +0300

    Редактирование шапки и начало верстки следующего блока

---
+++ src/components/RegButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const RegButton = () => {
+  return (
+    <div>
+      <button className="reg-button">Записаться на ТО</button>
+    </div>
+  );
+};
---
+++ src/components/ReqDocuments.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ReqDocuments = () => {
+  return (
+    <div id="documents">
+      <div className="required-documents">
+        <h1>При себе необходимо иметь:</h1>
+        <hr></hr>
+      </div>
+    </div>
+  );
+};
