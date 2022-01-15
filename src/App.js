7492e6e6c2c8dd97bdd5d98ed4be109b8bf50e0d
Max Lebedev <dev.runny@gmail.com>
2022-01-15 18:38:11 +0300

    Редактирование шапки и начало верстки следующего блока

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./scss/App.scss";
 import "./scss/fonts.scss";
-import Header from "./components/Header";
+import { Header } from "./components/Header";
+import { ReqDocuments } from "./components/ReqDocuments";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ReqDocuments />
     </div>
   );
 }
